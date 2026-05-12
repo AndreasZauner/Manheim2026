50bd30b0401b5e3fa11d91253254a94563cf825a
AndreasZauner <andreas-zauner@gmx.de>
2026-05-12 18:34:00 +0200

    Clean config cache buster update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-﻿window.APP_CONFIG = {
+window.APP_CONFIG = {
   SUPABASE_URL: 'https://qpondbcuoyaxsidyzddn.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_bdwHq4KzgvI0t8R2vTGYIQ_ezvqeG_8',
   PROJECT_SLUG: 'lehrgrabung-kerpen-manheim-2026'
@@ -327,6 +327,5 @@ function wait(ms) {
   }
   function escapeHtml(value) { return String(value ?? '').replaceAll('&','&amp;').replaceAll('<','&lt;').replaceAll('>','&gt;').replaceAll('"','&quot;').replaceAll("'", '&#39;'); }
 })();
-
 
 
